4449a388223da46fb3d3e1915f66391b133f29c2
Amir-Shivasokhan <shivasokhana@gmail.com>
2025-02-07 10:47:13 +0100

    fetched data and APIs

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,12 +32,16 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw[]>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
+   const data = await sql<LatestInvoiceRaw[]>`
+  SELECT DISTINCT ON (customers.id) 
+    invoices.amount, 
+    customers.name, 
+    customers.image_url, 
+    customers.email
+FROM invoices
+JOIN customers ON invoices.customer_id = customers.id
+ORDER BY customers.id, invoices.date DESC
+LIMIT 5`;
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
